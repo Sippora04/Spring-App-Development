d68c3d7d653cabe8cdc24e50f5cb725df04b663d
Sippora04 <sippora.toppo95@gmail.com>
2020-12-19 04:55:14 +0530

    Added: dependency injection using autowired annotation

--- main/java/com/example/demo/springconcepts/SpringConceptsApplication.java
+++ main/java/com/example/demo/springconcepts/SpringConceptsApplication.java
@@ -5,16 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import com.example.demo.springconcepts.component.DemoBean;
+import com.example.demo.springconcepts.component.EmployeeBean;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
 	public static final Logger logger = LoggerFactory.getLogger(SpringApplication.class);
 
 	public static void main(String[] args) {
-		System.out.println(" *** Welcome to Spring Concept Demo *** ");
+		logger.debug(" *** Welcome to Spring Concept Demo *** ");
 		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
 		logger.debug("Checking context: {}", context.getBean(DemoBean.class));
 		logger.debug("\n*** Example Using @Autowire annotation on property ***");
+		EmployeeBean employeeBean = context.getBean(EmployeeBean.class);
+		employeeBean.setEid(8104);
+		employeeBean.setEname("Spring Framework Application");
+		employeeBean.showEmployeeDetails();
 	}
 }
